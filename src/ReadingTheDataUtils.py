6e66c843de9acc6c6b2af7706561a35a04dced6f
Yarden <yardenrachamim1993@gmail.com>
2019-12-13 14:07:50 +0200

    updated the lables

--- src/ReadingTheDataUtils.py
+++ src/ReadingTheDataUtils.py
@@ -14,7 +14,9 @@ def get_feature_names(df: pd.DataFrame, label_names):
             lambda x: x not in label_names
             and x != 'label_source'
             and x != 'timestamp'
-            and x != 'label_name', df.columns
+            and x != 'label_name'
+            and not x.startswith('label'),
+            df.columns
         )
     )
 
